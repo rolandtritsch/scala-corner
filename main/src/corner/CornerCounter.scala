b8e4052a751b31abff8cea911ad054128b56bf6c
Roland Tritsch <roland@tritsch.email>
2025-01-02 06:00:20 +0000

    Make CI/CD pass.

--- main/src/corner/CornerCounter.scala
+++ main/src/corner/CornerCounter.scala
@@ -2,9 +2,10 @@ package corner
 
 type Position = (Int, Int)
 
+/** The CornerCounter. */
 class CornerCounter[T](val regions: Map[Position, T]) {
   val corners = regions.view.mapValues(_.toString.toInt)
-  
+
   def this(grid: Array[Array[T]])(using fromGrid: Array[Array[T]] => Map[Position, T]) = this(fromGrid(grid))
   def count(p: Position) = corners(p)
 }
@@ -18,6 +19,7 @@ object CornerCounter {
     positions.toMap
   }
 
+  /** @return A CornerCounter initialized with the contents of the given resource. */
   def fromResource(path: String): CornerCounter[Char] = {
     val source = scala.io.Source.fromResource(path)
     val grid = source.getLines().map(_.toCharArray()).toArray
